faf64aefb6362cf7b757fe72cfaf38fd1d1a5562
Maximilian Tagher <feedback.tagher@gmail.com>
2013-08-09 10:43:54 -0700

    Initial Commit

---
+++ PlayThrough/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PlayThrough
+//
+//  Created by Maximilian Tagher on 8/9/13.
+//  Copyright (c) 2013 Tagher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
